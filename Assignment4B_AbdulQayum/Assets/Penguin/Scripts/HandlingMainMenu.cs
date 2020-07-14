47a3d42af410b3116e05512ab271b7b6436004e0
Abdul Qayum <bitf13a039.uos@gmail.com>
2020-07-14 21:13:25 +0500

    Project Upload to GitHub.

---
+++ Assignment4B_AbdulQayum/Assets/Penguin/Scripts/HandlingMainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HandlingMainMenu : MonoBehaviour
+{
+    public GameObject panelofmainmenu;
+    public GameObject panelofhummingbird;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelofmainmenu.SetActive(true);
+    }
+    public void Penguingame()
+    {
+        SceneManager.LoadScene("Penguins");
+    }
+    public void Hummingbirdmenu()
+    {
+        panelofhummingbird.SetActive(true);
+        panelofmainmenu.SetActive(false);
+    }
+    public void MlAgent()
+    {
+        SceneManager.LoadScene("Training");
+    }
+    public void Battlewithmlagent()
+    {
+        SceneManager.LoadScene("FlowerIsland");
+    }
+    public void gobacktmainpanel()
+    {
+        panelofmainmenu.SetActive(true);
+        panelofhummingbird.SetActive(false);
+
+    }
+
+}
